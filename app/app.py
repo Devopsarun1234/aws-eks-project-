d0c62a915ca406612061c1aec2c0986bd5e6220a
Arun <tripathyarun0812@gmail.com>
2025-11-09 15:27:34 +0530

    Initial commit - AWS EKS Flask WebApp project

---
+++ app/app.py
@@ -0,0 +1,11 @@
+echo "from flask import Flask, jsonify
+
+app = Flask(__name__)
+
+@app.route('/')
+def home():
+    return jsonify({'message': 'Welcome to AWS EKS WebApp!'})
+
+if __name__ == '__main__':
+    app.run(host='0.0.0.0', port=5000)" > app/app.py
+
